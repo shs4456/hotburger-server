09ca2a172ea09f7855c053c075766cfb77a6165d
shs4456 <yusol1101@gmail.com>
2026-04-08 04:36:02 +0900

    Update server.py

--- server.py
+++ server.py
@@ -4,35 +4,50 @@ from flask_sock import Sock
 app = Flask(__name__)
 sock = Sock(app)
 
-clients = []
+clients = set()
+
 
 @app.route("/")
 def home():
     return "HOTBURGER WebSocket Server Alive"
 
+
 @sock.route("/ws")
 def websocket(ws):
-    clients.append(ws)
-    print("클라이언트 연결됨")
+    clients.add(ws)
+    print("클라이언트 연결됨:", len(clients))
 
     try:
         while True:
             data = ws.receive()
 
-            if not data:
+            if data is None:
                 break
 
-            print("메시지 수신")
+            print("메시지 수신:", str(data)[:50])
 
-            for c in clients:
+            dead = []
+            for c in list(clients):
+                if c is ws:
+                    continue
                 try:
                     c.send(data)
                 except:
+                    dead.append(c)
+
+            for d in dead:
+                try:
+                    clients.remove(d)
+                except:
                     pass
 
-    except:
-        pass
+    except Exception as e:
+        print("에러:", e)
 
     finally:
-        clients.remove(ws)
-        print("연결 종료")
+        try:
+            clients.remove(ws)
+        except:
+            pass
+
+        print("연결 종료:", len(clients))
